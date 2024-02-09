4802bcca77a277be417e02d782e56994f337eaa9
Iury Silveira Marques <iurysmarques0@gmail.com>
2024-02-09 16:43:13 -0300

    mudança de slides manual
    Esta dando um bug

--- slider/slider.js
+++ slider/slider.js
@@ -4,8 +4,11 @@ const slider = document.querySelector('.slider-images');
 
 const images = Array.from(slider.children);
 
-
 let imageIndex=0;
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateSlider()
+});
 
 function updateSlider(){
     images.forEach(image=>{
@@ -44,9 +47,48 @@ function updateSlider(){
     imageIndex=(imageIndex+1)%images.length;
 }
 
+
+
+function prevSlide(){
+    images.forEach(image=>{
+        image.classList.remove('active','previous','next','inactive');
+    });
+
+    images[imageIndex].classList.add('active');
+
+    if (imageIndex + 1 < images.length) {
+        images[imageIndex + 1].classList.add('next');
+    } else {
+        images[0].classList.add('next');
+    }
+    
+    if (imageIndex - 1 >= 0) {
+        images[imageIndex - 1].classList.add('previous');
+    } else {
+        images[images.length - 1].classList.add('previous');
+    }
+
+    images.forEach((image,index)=>{
+        if(index!==imageIndex&&index!==(imageIndex-1+images.length)%images.length&&index!==(imageIndex+1)%images.length){
+            image.classList.add('inactive');
+        }
+    });
+
+    backgrounds.forEach((background)=>{
+        background.style.opacity = 0;
+    });
+
+    if(images[imageIndex].classList.contains('active')){
+        backgrounds[imageIndex].style.opacity = 1;
+    }
+
+    imageIndex = (imageIndex - 1 + images.length) % images.length;
+}
+
+
 updateSlider()
 
-setInterval(updateSlider,9000);
+//setInterval(updateSlider,5000);
 
 
 console.log(images[3].getAttribute('alt'))
