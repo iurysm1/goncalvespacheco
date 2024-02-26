f1477f35bfad92eef444c0dcd9e2ad06efca5954
Iury Silveira Marques <iurysmarques0@gmail.com>
2024-02-26 16:43:42 -0300

    90% da responsivdade do slider pronta
    Terminando os ultimos detalhes

--- slider/slider2.js
+++ slider/slider2.js
@@ -9,6 +9,8 @@ let pauserSlide;
 
 var nome = document.getElementById('nomeFuncionario');
 var sobre = document.getElementById('sobreFuncionario');
+var nomeMobile = document.getElementById('nomeFuncionarioMobile');
+var sobreMobile = document.getElementById('sobreFuncionarioMobile');
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -44,7 +46,6 @@ function updateSlider(){
 
     
     images.forEach(imagem=>{
-    console.log(imagem.classList.contains('next')&&imagem.classList.contains('previous')&&!imagem.classList.contains('active'));
         if(!imagem.classList.contains('next')&&!imagem.classList.contains('previous')&&!imagem.classList.contains('active')){
             imagem.classList.add('inactive');
         }
@@ -79,7 +80,6 @@ function prevSlide(){
         image.classList.remove('active','previous','next','inactive');
     });
 
-    console.log("index active: "+imageIndex)
     images[imageIndex].classList.add('active');
 
     
@@ -147,6 +147,8 @@ function trocarDados(){
             case 'luana':
                 nome.style.opacity=0;
                 sobre.style.opacity=0;
+                nomeMobile.style.opacity=0;
+                sobreMobile.style.opacity=0;
                 facebook.style.opacity=0;
                 instagram.style.opacity=0;
                 facebook.href="https://instagram.com/luanamarques0";
@@ -155,10 +157,15 @@ function trocarDados(){
                 setTimeout(() => {
                 nome.style.opacity=1;
                 sobre.style.opacity=1;
+                nomeMobile.style.opacity=1;
+                sobreMobile.style.opacity=1;
                 facebook.style.opacity=1;
                 instagram.style.opacity=1;
                 nome.textContent="LUANA SILVEIRA MARQUES"
                 sobre.textContent="Advogada. Especialista em Direito Imobiliário. Formada pela Universidade do Vale do Itajaí - UNIVALI. Inscrita na Ordem dos Advogados do Brasil com o nº 49.290. Pós-graduada em Direito e Negócios Imobiliários pelo Complexo de Ensino Superior de Santa Catarina - CESUSC. Pós graduanda em Direito Ambiental e Urbanístico. Direito Tributário - Fundação Getúlio Vargas - FGV Auditora do TJD - Tribunal de Justiça do Futebol de SC. Ex-Presidente da Subcomissão Jovem Advocacia da 30° Subseção - OAB/SC. Ex-Presidente da Comissão de Direito Imobiliário da 30° Subseção - OAB/SC."
+
+                nomeMobile.textContent="LUANA SILVEIRA MARQUES"
+                sobreMobile.textContent="Advogada. Especialista em Direito Imobiliário. Formada pela Universidade do Vale do Itajaí - UNIVALI. Inscrita na Ordem dos Advogados do Brasil com o nº 49.290. Pós-graduada em Direito e Negócios Imobiliários pelo Complexo de Ensino Superior de Santa Catarina - CESUSC. Pós graduanda em Direito Ambiental e Urbanístico. Direito Tributário - Fundação Getúlio Vargas - FGV Auditora do TJD - Tribunal de Justiça do Futebol de SC. Ex-Presidente da Subcomissão Jovem Advocacia da 30° Subseção - OAB/SC. Ex-Presidente da Comissão de Direito Imobiliário da 30° Subseção - OAB/SC."
                 }, 900);
                 
                 break;
@@ -166,30 +173,45 @@ function trocarDados(){
             case 'marlon':
                 nome.style.opacity=0;
                 sobre.style.opacity=0;
+                nomeMobile.style.opacity=0;
+                sobreMobile.style.opacity=0;
                 facebook.style.opacity=0;
                 instagram.style.opacity=0;
                 setTimeout(() => {
                 nome.style.opacity=1;
                 sobre.style.opacity=1;
+                nomeMobile.style.opacity=1;
+                sobreMobile.style.opacity=1;
                 facebook.style.opacity=1;
                 instagram.style.opacity=1;
                 nome.textContent="MARLON TESTONI BATISTI"
                 sobre.textContent="Sócio. Formado pela Universidade do Sul de Santa Catarina - UNISUL. Inscrito na Ordem dos Advogados do Brasil com o n. 32.631. Pós-graduado em Direito Material e Processual do Trabalho pela Faculdade de Direito Damásio de Jesus. Pós-graduado em Direito Constitucional pela Anhanguera. Ex-Assessor Jurídico do Município de Imbituba/SC. Ex-Procurador do Município de Imbituba. Ex-Assessor Jurídico da Presidência da Câmara Municipal de Imbituba. Membro da Associação Catarinense dos Advogados Trabalhistas - ACAT. Delegado da Caixa de Assistência dos Advogados de Santa Catarina (gestão 2019/2021 e 2022/2024). Diretor Jurídico da Associação Empresarial de Imbituba desde 2019."
+
+                nomeMobile.textContent="MARLON TESTONI BATISTI"
+                sobreMobile.textContent="Sócio. Formado pela Universidade do Sul de Santa Catarina - UNISUL. Inscrito na Ordem dos Advogados do Brasil com o n. 32.631. Pós-graduado em Direito Material e Processual do Trabalho pela Faculdade de Direito Damásio de Jesus. Pós-graduado em Direito Constitucional pela Anhanguera. Ex-Assessor Jurídico do Município de Imbituba/SC. Ex-Procurador do Município de Imbituba. Ex-Assessor Jurídico da Presidência da Câmara Municipal de Imbituba. Membro da Associação Catarinense dos Advogados Trabalhistas - ACAT. Delegado da Caixa de Assistência dos Advogados de Santa Catarina (gestão 2019/2021 e 2022/2024). Diretor Jurídico da Associação Empresarial de Imbituba desde 2019."
                 }, 900);
                 
                 break;
             case 'felipe':
                 nome.style.opacity=0;
                 sobre.style.opacity=0;
+                nomeMobile.style.opacity=0;
+                sobreMobile.style.opacity=0;
                 facebook.style.opacity=0;
                 instagram.style.opacity=0;
                 setTimeout(() => {
                 nome.style.opacity=1;
                 sobre.style.opacity=1;
+                nomeMobile.style.opacity=1;
+                sobreMobile.style.opacity=1;
                 facebook.style.opacity=1;
                 instagram.style.opacity=1;
                 nome.textContent="FELIPE MARLONDREY BALTAZAR CARDOSO"
                 sobre.textContent="Advogado, Formado pelo Centro Universitário Barriga Verde – UNIBAVE, Inscrito na Ordem dos Advogados do Brasil sob o nº 51.237. Com atuação profissional no Município de Treze de Maio e Região. Pós-Graduando em Direito Administrativo – Damásio Educacional, Criciúma/SC. Membro da Comissão de Direito Público da 6ª Subseção da OAB/SC – Tubarão."
+
+                nomeMobile.textContent="FELIPE MARLONDREY BALTAZAR CARDOSO"
+                sobreMobile.textContent="Advogado, Formado pelo Centro Universitário Barriga Verde – UNIBAVE, Inscrito na Ordem dos Advogados do Brasil sob o nº 51.237. Com atuação profissional no Município de Treze de Maio e Região. Pós-Graduando em Direito Administrativo – Damásio Educacional, Criciúma/SC. Membro da Comissão de Direito Público da 6ª Subseção da OAB/SC – Tubarão."
+                
                 }, 900);
                 
                 break;
