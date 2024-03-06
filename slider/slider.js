9225be41527852f454e9b19dfe69855e98f224d8
Iury Silveira Marques <iurysmarques0@gmail.com>
2024-03-06 09:56:52 -0300

    Ajeitando slider

--- slider/slider.js
+++ slider/slider.js
@@ -15,12 +15,14 @@ var nome = document.getElementById('nomeFuncionario');
 var sobre = document.getElementById('sobreFuncionario');
 var nomeMobile = document.getElementById('nomeFuncionarioMobile');
 var sobreMobile = document.getElementById('sobreFuncionarioMobile');
-var elementosOpacityAnimation=[iconSobreMobile,nome,sobre,nomeMobile,sobreMobile];
+var cargo = document.getElementById('cargoFuncionario');
+var cargoMobile = document.getElementById('cargoFuncionarioMobile');
+var elementosOpacityAnimation=[iconSobreMobile,nome,sobre,nomeMobile,sobreMobile, cargo, cargoMobile];
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    nome.style.opacity=1;
-    sobre.style.opacity=1;
+    
+    setOpacity(1)
 });
 
 function updateSlider(){
@@ -152,6 +154,9 @@ function setOpacity(opactiynumber){
 function trocarDados(){
     var facebook = document.getElementById('LinkFacebookSlider');
     var instagram = document.getElementById('LinkInstagramSlider');
+
+    var facebookMobile=document.getElementById('facebookMobileSlider')
+    var instagramMobile=document.getElementById('instagramMobileSlider')
     
     
     
@@ -163,44 +168,252 @@ function trocarDados(){
                 setOpacity(0)
                 facebook.href="https://instagram.com/luanamarques0";
                 instagram.href="https://instagram.com/luanamarques0";
+                facebookMobile.href="https://instagram.com/luanamarques0";
+                instagramMobile.href="https://instagram.com/luanamarques0";
                 
                 setTimeout(() => {
                 setOpacity(1)
                 nome.textContent="LUANA SILVEIRA MARQUES"
-                sobre.textContent="Advogada. Especialista em Direito Imobiliário. Formada pela Universidade do Vale do Itajaí - UNIVALI. Inscrita na Ordem dos Advogados do Brasil com o nº 49.290. Pós-graduada em Direito e Negócios Imobiliários pelo Complexo de Ensino Superior de Santa Catarina - CESUSC. Pós graduanda em Direito Ambiental e Urbanístico. Direito Tributário - Fundação Getúlio Vargas - FGV Auditora do TJD - Tribunal de Justiça do Futebol de SC. Ex-Presidente da Subcomissão Jovem Advocacia da 30° Subseção - OAB/SC. Ex-Presidente da Comissão de Direito Imobiliário da 30° Subseção - OAB/SC."
+                sobre.textContent="Formada em Direito pela Universidade do Vale do Itajaí, pós-graduada em Direito e Negócios Imobiliários, pós graduada em Direito Ambiental, pós graduanda em Direito Processual Civil."
+                cargo.textContent="Sócia";    
+                
 
                 nomeMobile.textContent="LUANA SILVEIRA MARQUES"
-                sobreMobile.textContent="Advogada. Especialista em Direito Imobiliário. Formada pela Universidade do Vale do Itajaí - UNIVALI. Inscrita na Ordem dos Advogados do Brasil com o nº 49.290. Pós-graduada em Direito e Negócios Imobiliários pelo Complexo de Ensino Superior de Santa Catarina - CESUSC. Pós graduanda em Direito Ambiental e Urbanístico. Direito Tributário - Fundação Getúlio Vargas - FGV Auditora do TJD - Tribunal de Justiça do Futebol de SC. Ex-Presidente da Subcomissão Jovem Advocacia da 30° Subseção - OAB/SC. Ex-Presidente da Comissão de Direito Imobiliário da 30° Subseção - OAB/SC."
+                sobreMobile.textContent="Formada em Direito pela Universidade do Vale do Itajaí, pós-graduada em Direito e Negócios Imobiliários, pós graduada em Direito Ambiental, pós graduanda em Direito Processual Civil."
+                cargoMobile.textContent="Sócia";
                 }, 900);
                 
                 break;
 
             case 'marlon':
                 setOpacity(0)
+                facebook.href="https://instagram.com/luanamarques0";
+                instagram.href="https://instagram.com/marlonbatisti";
+                facebookMobile.href="https://instagram.com/luanamarques0";
+                instagramMobile.href="https://instagram.com/marlonbatisti";
                 setTimeout(() => {
                 setOpacity(1)
+
                 nome.textContent="MARLON TESTONI BATISTI"
-                sobre.textContent="Sócio. Formado pela Universidade do Sul de Santa Catarina - UNISUL. Inscrito na Ordem dos Advogados do Brasil com o n. 32.631. Pós-graduado em Direito Material e Processual do Trabalho pela Faculdade de Direito Damásio de Jesus. Pós-graduado em Direito Constitucional pela Anhanguera. Ex-Assessor Jurídico do Município de Imbituba/SC. Ex-Procurador do Município de Imbituba. Ex-Assessor Jurídico da Presidência da Câmara Municipal de Imbituba. Membro da Associação Catarinense dos Advogados Trabalhistas - ACAT. Delegado da Caixa de Assistência dos Advogados de Santa Catarina (gestão 2019/2021 e 2022/2024). Diretor Jurídico da Associação Empresarial de Imbituba desde 2019."
+                sobre.textContent="Formado em Direito pela Universidade do Sul de Santa Catarina, possui especialização em Direito Material e Processual do Trabalho, e especialização em Direito Constitucional."
+                cargo.textContent="Sócio"
 
                 nomeMobile.textContent="MARLON TESTONI BATISTI"
-                sobreMobile.textContent="Sócio. Formado pela Universidade do Sul de Santa Catarina - UNISUL. Inscrito na Ordem dos Advogados do Brasil com o n. 32.631. Pós-graduado em Direito Material e Processual do Trabalho pela Faculdade de Direito Damásio de Jesus. Pós-graduado em Direito Constitucional pela Anhanguera. Ex-Assessor Jurídico do Município de Imbituba/SC. Ex-Procurador do Município de Imbituba. Ex-Assessor Jurídico da Presidência da Câmara Municipal de Imbituba. Membro da Associação Catarinense dos Advogados Trabalhistas - ACAT. Delegado da Caixa de Assistência dos Advogados de Santa Catarina (gestão 2019/2021 e 2022/2024). Diretor Jurídico da Associação Empresarial de Imbituba desde 2019."
+                sobreMobile.textContent="Formado em Direito pela Universidade do Sul de Santa Catarina, possui especialização em Direito Material e Processual do Trabalho, e especialização em Direito Constitucional."
+                cargoMobile.textContent="Sócio"
                 }, 900);
                 
                 break;
             case 'felipe':
                 setOpacity(0)
+                facebook.href="https://instagram.com/luanamarques0";
+                instagram.href="https://instagram.com/cardosofmb";
+                
+                facebookMobile.href="https://instagram.com/luanamarques0";
+                instagramMobile.href="https://instagram.com/cardosofmb";
+
                 setTimeout(() => {
                 setOpacity(1)
                 nome.textContent="FELIPE MARLONDREY BALTAZAR CARDOSO"
-                sobre.textContent="Advogado, Formado pelo Centro Universitário Barriga Verde – UNIBAVE, Inscrito na Ordem dos Advogados do Brasil sob o nº 51.237. Com atuação profissional no Município de Treze de Maio e Região. Pós-Graduando em Direito Administrativo – Damásio Educacional, Criciúma/SC. Membro da Comissão de Direito Público da 6ª Subseção da OAB/SC – Tubarão."
+                sobre.textContent="Formado em Direito pelo Centro Universitário Barriga Verde, pós-graduado em Direito Administrativo."
+                cargo.textContent="Associado"
+
 
                 nomeMobile.textContent="FELIPE MARLONDREY BALTAZAR CARDOSO"
-                sobreMobile.textContent="Advogado, Formado pelo Centro Universitário Barriga Verde – UNIBAVE, Inscrito na Ordem dos Advogados do Brasil sob o nº 51.237. Com atuação profissional no Município de Treze de Maio e Região. Pós-Graduando em Direito Administrativo – Damásio Educacional, Criciúma/SC. Membro da Comissão de Direito Público da 6ª Subseção da OAB/SC – Tubarão."
+                sobreMobile.textContent="Formado em Direito pelo Centro Universitário Barriga Verde, pós-graduado em Direito Administrativo."
+                cargoMobile.textContent="Associado"
+
                 
                 }, 900);
                 
                 break;
+            
+            case 'orlando':
+                setOpacity(0)
+                facebook.href="https://instagram.com/luanamarques0";
+                instagram.href="https://instagram.com/orlandogoncalvespa";
                 
+                facebookMobile.href="https://instagram.com/luanamarques0";
+                instagramMobile.href="https://instagram.com/orlandogoncalvespa";
+
+                setTimeout(() => {
+                setOpacity(1)
+                nome.textContent="ORLANDO GONÇALVES PACHECO JÚNIOR"
+                sobre.textContent="Formado em Direito pela Universidade do Sul de Santa Catarina, Pós-graduado em Direito Processual Civil e com especialidade em Direito Empresarial."
+                cargo.textContent="Sócio Fundador"
+
+
+                nomeMobile.textContent="ORLANDO GONÇALVES PACHECO JÚNIOR"
+                sobreMobile.textContent="Formado em Direito pela Universidade do Sul de Santa Catarina, Pós-graduado em Direito Processual Civil e com especialidade em Direito Empresarial."
+                cargoMobile.textContent="Sócio Fundador"
+
+                
+                }, 900);
+                
+                break;
+
+                case 'eduardo':
+                    setOpacity(0)
+                    facebook.href="https://instagram.com/luanamarques0";
+                    instagram.href="https://instagram.com/edufaustina";
+
+                    facebookMobile.href="https://instagram.com/luanamarques0";
+                    instagramMobile.href="https://instagram.com/edufaustina";
+                    setTimeout(() => {
+                    setOpacity(1)
+                    nome.textContent="EDUARDO FAUSTINA DA ROSA"
+                    sobre.textContent="Formado em Direito pela Universidade do Sul de Santa Catarina, possuí especialização em Direito Material e Processual Penal e formação em Agente de Desenvolvimento Regional."
+                    cargo.textContent="Sócio"
+    
+    
+                    nomeMobile.textContent="EDUARDO FAUSTINA DA ROSA"
+                    sobreMobile.textContent="Formado em Direito pela Universidade do Sul de Santa Catarina, possuí especialização em Direito Material e Processual Penal e formação em Agente de Desenvolvimento Regional."
+                    cargoMobile.textContent="Sócio"
+    
+                    
+                    }, 900);
+                    
+                    break;
+
+                case 'guilherme':
+                    setOpacity(0)
+                    facebook.href="https://instagram.com/luanamarques0";
+                    instagram.href="https://instagram.com/adv.guilhermetavares";
+                    
+                    facebookMobile.href="https://instagram.com/luanamarques0";
+                    instagramMobile.href="https://instagram.com/adv.guilhermetavares";
+                    
+                    setTimeout(() => {
+                    setOpacity(1)
+                    nome.textContent="GUILHERME TAVARES DE JESUS"
+                    sobre.textContent="Formado em Direito pela Universidade do Sul de Santa Catarina, possui especialização em Direito Material Civil."
+                    cargo.textContent="Associado"
+    
+    
+                    nomeMobile.textContent="GUILHERME TAVARES DE JESUS"
+                    sobreMobile.textContent="Formado em Direito pela Universidade do Sul de Santa Catarina, possui especialização em Direito Material Civil."
+                    cargoMobile.textContent="Associado"
+    
+                    
+                    }, 900);
+                    
+                    break;
+                case 'joao':
+                    setOpacity(0)
+                    facebook.href="https://instagram.com/luanamarques0";
+                    instagram.href="https://instagram.com/jgabrielkuntze";
+                    
+                    facebookMobile.href="https://instagram.com/luanamarques0";
+                    instagramMobile.href="https://instagram.com/jgabrielkuntze";
+
+                    setTimeout(() => {
+                    setOpacity(1)
+                    nome.textContent="JOÃO GABRIEL KUNTZE"
+                    sobre.textContent="Formado em Direito pelo Complexo de Ensino Superior de Santa Catarina, pós-graduado em Direito Material Criminal."
+                    cargo.textContent="Associado"
+    
+    
+                    nomeMobile.textContent="JOÃO GABRIEL KUNTZE"
+                    sobreMobile.textContent="Formado em Direito pelo Complexo de Ensino Superior de Santa Catarina, pós-graduado em Direito Material Criminal."
+                    cargoMobile.textContent="Associado"
+    
+                    
+                    }, 900);
+                    
+                    break;
+                
+                case 'melina':
+                    setOpacity(0)
+                    facebook.href="https://instagram.com/luanamarques0";
+                    instagram.href="https://instagram.com/melinafechine";
+                    
+                    facebookMobile.href="https://instagram.com/luanamarques0";
+                    instagramMobile.href="https://instagram.com/melinafechine";
+                    
+                    setTimeout(() => {
+                    setOpacity(1)
+                    nome.textContent="MELINA FECHINE MODOLON FILETI"
+                    sobre.textContent="Formada em Direito pela Universidade do Sul de Santa Catarina, pós-graduada em Direito Material e Processual Tributário."
+                    cargo.textContent="Associada"
+    
+    
+                    nomeMobile.textContent="MELINA FECHINE MODOLON FILETI"
+                    sobreMobile.textContent="Formada em Direito pela Universidade do Sul de Santa Catarina, pós-graduada em Direito Material e Processual Tributário."
+                    cargoMobile.textContent="Associada"
+    
+                    
+                    }, 900);
+                    
+                    break;
+                case 'isabella':
+                    setOpacity(0)
+                    facebook.href="https://instagram.com/luanamarques0";
+                    instagram.href="https://instagram.com/isabella.testoni";
+                    
+                    facebookMobile.href="https://instagram.com/luanamarques0";
+                    instagramMobile.href="https://instagram.com/isabella.testoni";
+                    
+                    setTimeout(() => {
+                    setOpacity(1)
+                    nome.textContent="ISABELLA PEREIRA TESTONI BATISTI"
+                    sobre.textContent="Formada em Direito pela Universidade do Sul de Santa Catarina, pós-graduada em Direito Material Empresarial e Contratual, graduanda em Direito do Trabalho."
+                    cargo.textContent="Associada"
+
+
+                    nomeMobile.textContent="ISABELLA PEREIRA TESTONI BATISTI"
+                    sobreMobile.textContent="Formada em Direito pela Universidade do Sul de Santa Catarina, pós-graduada em Direito Material Empresarial e Contratual, graduanda em Direito do Trabalho."
+                    cargoMobile.textContent="Associada"
+
+                    
+                    }, 900);
+                
+                    break;
+                case 'anna':
+                    setOpacity(0)
+                    facebook.href="https://instagram.com/luanamarques0";
+                    instagram.href="https://instagram.com/anna.attanasio0";
+                    
+                    facebookMobile.href="https://instagram.com/luanamarques0";
+                    instagramMobile.href="https://instagram.com/anna.attanasio0";
+                    
+                    setTimeout(() => {
+                    setOpacity(1)
+                    nome.textContent="ANNA KAROLINA ATTANASIO"
+                    sobre.textContent="Formada em Direito pela Universidade do Sul de Santa Catarina, pós-graduada em Direito de Família e Direito Imobiliário."
+                    cargo.textContent="Associada"
+
+
+                    nomeMobile.textContent="ANNA KAROLINA ATTANASIO"
+                    sobreMobile.textContent="Formada em Direito pela Universidade do Sul de Santa Catarina, pós-graduada em Direito de Família e Direito Imobiliário."
+                    cargoMobile.textContent="Associada"
+
+                    
+                    }, 900);
+                
+                    break; 
+                    case 'suzani':
+                        setOpacity(0)
+                        facebook.href="https://instagram.com/luanamarques0";
+                        instagram.href="https://instagram.com/suzanifaustina";
+                        
+                        facebookMobile.href="https://instagram.com/luanamarques0";
+                        instagramMobile.href="https://instagram.com/suzanifaustina";
+                        
+                        setTimeout(() => {
+                        setOpacity(1)
+                        nome.textContent="SUZANI FAUSTINA DA ROSA"
+                        sobre.textContent="Formada em Direito pela Universidade do Sul de Santa Catarina, pós-graduanda em Advocacia Criminal."
+                        cargo.textContent="Associada"
+    
+    
+                        nomeMobile.textContent="SUZANI FAUSTINA DA ROSA"
+                        sobreMobile.textContent="Formada em Direito pela Universidade do Sul de Santa Catarina, pós-graduanda em Advocacia Criminal."
+                        cargoMobile.textContent="Associada"
+    
+                        
+                        }, 900);
+                    
+                        break; 
         }
         }
         
