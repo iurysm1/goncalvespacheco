7a77eca6a589ccd004f1544c20d51fdc17b075c8
Iury Silveira Marques <iurysmarques0@gmail.com>
2024-03-15 16:58:34 -0300

    Section do primeiro slogan terminado

--- home/primeiroSlogan/primeiroSlogan.js
+++ home/primeiroSlogan/primeiroSlogan.js
@@ -5,15 +5,18 @@ import * as utilidades from "/utilities.js";
     const h1Slogan = document.getElementById('fraseH1');
     const h2Slogan  = document.getElementById('fraseH2');
     const h3Slogan  = document.getElementById('fraseH3');
+    const quemSomosBotao= document.getElementById('quemSomosBotao')
 
     if (utilidades.isElementVisibleExport(h1Slogan)) {
       h1Slogan.classList.add('h1slide');
       h2Slogan.classList.add('h2slide');
       h3Slogan.classList.add('h3slide');
+      quemSomosBotao.classList.add('h1slide')
     }else{
         h1Slogan.classList.remove('h1slide');
         h2Slogan.classList.remove('h2slide');
         h3Slogan.classList.remove('h3slide');
+        quemSomosBotao.classList.remove('h1slide')
     }
   }
   
